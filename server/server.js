865cb403cec8ab90c207e9d39620e8c693f289de
jhnmeelr <jhn.meelr@gmail.com>
2016-12-09 20:58:58 +0200

    Getting an Individual Resource - GET _todos_ -id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { ObjectID } from 'mongodb';
 
 import mongoose from './db/mongoose';
 
@@ -13,6 +14,23 @@ app.use(bodyParser.json());
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ todos });
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(req.params.id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
     }, (err) => {
         res.status(400).send(err);
     });
