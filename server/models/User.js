a90fa0805844ea1ab14c6ec7ffccef7ef220ace9
jhnmeelr <jhn.meelr@gmail.com>
2016-12-12 12:36:05 +0200

    Logging In - POST _users_login

--- server/models/User.js
+++ server/models/User.js
@@ -74,6 +74,25 @@ UserSchema.statics = {
             'tokens.token': token,
             'tokens.access': 'auth'
         })
+    },
+    findByCredentials: function (email, password) {
+        let User = this;
+
+        return User.findOne({ email }).then((user) => {
+            if (!user) {
+                return Promise.reject();
+            }
+
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (res) {
+                        resolve(user)
+                    } else {
+                        reject(err);
+                    }
+                });
+            });
+        });
     }
 }
 
